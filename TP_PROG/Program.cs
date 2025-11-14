1b74c749d45cffc325dd00839b1288cefa838b2f
Rosslan <2551221@cstjean.qc.ca>
2025-11-13 20:57:56 -0500

    Add project files.

---
+++ TP_PROG/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TP_PROG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ont commence le code ici");
+        }
+    }
+}
